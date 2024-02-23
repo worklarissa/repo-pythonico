7a986bb31d2f54992d499ee9a60bb5c5650420f2
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-23 09:45:06 -0300

    aula  alura

---
+++ aula1.py
@@ -0,0 +1,11 @@
+#print("ola aluno! bem vindo!!")
+
+print("Brasil", "ganhou", 5, "titulos mundiais", sep="-") #sep serve para indicar o separador da frase.
+
+print("Brasil", "ganhou", 5, "titulos mundiais", end="") #end é o que vai aparecer ao final da função
+
+dia = 15
+mes = 10
+ano = 2015
+
+print("/n", dia, mes, ano, sep="/" )
