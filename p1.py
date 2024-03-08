d3a2faf423402bd026d96a66c04dc9486b91c38a
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-03-07 21:50:57 -0300

    iniciei o curso do dunossauro

---
+++ p1.py
@@ -0,0 +1,2 @@
+#Faça um programa que imprima "Olá mundo" na tela
+print("Hello World!")
---
+++ p2.py
@@ -0,0 +1,6 @@
+#Faça um programa que pergunte o nome e a idade do usuário e o exiba na tela.abs
+nome = input('Digite seu nome: ')
+idade = int(input("Digite sua idade: "))
+
+# o f é de format de formatação
+print(f"Bem vindo {nome}, fico feliz em saber que você tem {idade} anos")
