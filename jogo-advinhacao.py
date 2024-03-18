b099a7002ea7c3ea7e81d34cec43540f9b3de8c3
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-18 10:25:18 -0300

    terminei a aula 3

--- jogo-advinhacao.py
+++ jogo-advinhacao.py
@@ -1,21 +1,24 @@
 print("Bem vindo ao jogo de Adivinhação!")
 
 numero_secreto = 42
+total_tentativas= 3
 
-chute_ = input("Digite o seu número: ")
-print("Você digitou: ", chute_)
-chute = int(chute_)
+while(total_tentativas > 0):
+    chute_ = input("Digite o seu número: ")
+    print("Você digitou: ", chute_)
+    chute = int(chute_)
+    
+    acertou = chute == numero_secreto
+    maior = chute < numero_secreto
+    menor = chute > numero_secreto
 
-acertou = chute == numero_secreto
-maior = chute < numero_secreto
-menor = chute > numero_secreto
+    if (acertou):
+        print("Você acertou")
+    else:
+        if (chute < numero_secreto):
+            print("Você errou, o número é maior")
+        elif (chute > numer_secreto):
+            print("Você errou, o numero secreto é menor")
 
-if (acertou):
-    print("Você acertou")
-else:
-    if (chute < numero_secreto):
-        print("Você errou, o número é maior")
-    elif (chute > numer_secreto):
-        print("Você errou, o numero secreto é menor")
 print("Fim de jogo")
 
